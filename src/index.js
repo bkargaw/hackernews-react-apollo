a33a872af4403894f448cdf3614fa55b7893bd00
Bruk Argaw <bkargaw@gmail.com>
2019-07-04 12:28:26 -0700

    added voting to links for authenticated user

--- src/index.js
+++ src/index.js
@@ -1,25 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/index.css';
-import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
 import { BrowserRouter } from 'react-router-dom'
 
 
-// 1
 import { ApolloProvider } from 'react-apollo'
 import { ApolloClient } from 'apollo-client'
 import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { setContext } from 'apollo-link-context'
 
+import './styles/index.css';
+import App from './components/App';
+import { AUTH_TOKEN } from './constants'
 
 
-// 2
-const httpLink = createHttpLink({
-  uri: 'http://localhost:4000'
-})
 
 const authLink = setContext((_, { headers }) => {
     const token = localStorage.getItem(AUTH_TOKEN)
@@ -31,13 +27,15 @@ const authLink = setContext((_, { headers }) => {
     }
   })
 
-// 3
+const httpLink = createHttpLink({
+uri: 'http://localhost:4000'
+})
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 })
 
-// 4
 ReactDOM.render(
   <BrowserRouter>
     <ApolloProvider client={client}>
